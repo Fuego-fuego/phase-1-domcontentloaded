899271f79a68ebaca43ca237b35d7b24aaa20aa0
nathaniEL mujesia <nathanielmujesia@gmail.com>
2024-01-08 13:05:11 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Your code goes here
+document.addEventListener('DOMContentLoaded', () => {
+    // select #text
+    const p = document.body.querySelector('#text');
+    // Change #text text content
+    p.textContent = "This is really cool!";
+})
